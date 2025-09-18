621a6273fcc13621a6f61939d995f7a496d33641
Samuel-Kugler <samuel-kugler@gmx.de>
2025-09-18 07:16:50 +0200

    refactored file to a better structure and help function for getting the json data

--- filter_users.py
+++ filter_users.py
@@ -1,9 +1,29 @@
 import json
+from json import JSONDecodeError
 
 
-def filter_users_by_name(name):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+def get_data() -> list:
+    """
+    Loads all the data from a json-file and returns its data
+    :return: list
+    """
+    try:
+        with open("users.json", "r") as file:
+            users = json.load(file)
+
+        return users
+    except FileNotFoundError:
+        return []
+    except JSONDecodeError:
+        return []
+
+
+def filter_users_by_name(name: str):
+    """
+    Displays the data of all persons with the name given.
+    :param name: str
+    """
+    users = get_data()
 
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
@@ -11,11 +31,34 @@ def filter_users_by_name(name):
         print(user)
 
 
-if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+def get_name() -> str:
+    """
+    Gets a valid name and returns it.
+    :return: str
+    """
+    while True:
+        name = input("Enter a name to filter users: ").strip()
 
-    if filter_option == "name":
-        name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
+        if not name.isalpha():
+            print(f"{name} is not a valid name! Only letters are valid.")
+            continue
+
+        return name
+
+
+commands = {
+    "name": {
+        "execute": filter_users_by_name,
+        "args": get_name
+    }
+}
+
+
+if __name__ == "__main__":
+    filter_option = (input(f"What would you like to filter by? (Currently, {commands.keys()} is supported): ").strip()
+                     .lower())
+
+    if filter_option in commands.keys():
+        commands[filter_option]["execute"](commands[filter_option]["args"]())
     else:
         print("Filtering by that option is not yet supported.")
